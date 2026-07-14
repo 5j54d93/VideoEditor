5b3a520b55fa6630360f4311e814ae17f86907d4
Ricky Chuang <5j54d93@gmail.com>
2026-07-14 22:35:27 +0800

    Initial Commit

---
+++ VideoEditor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VideoEditor
+//
+//  Created by 莊智凱 on 2026/7/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VideoEditor/VideoEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoEditorApp.swift
+//  VideoEditor
+//
+//  Created by 莊智凱 on 2026/7/14.
+//
+
+import SwiftUI
+
+@main
+struct VideoEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
